e8b523f1591d1fdbba3dd5de74f96833e6270b5e
lab314-10@labeng.local <lab314-10@labeng.local>
2020-01-24 15:46:26 +0700

    finished

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
-
+void printO(int N,int M){
+	int i=0,j=0;
+	if(M>0 && N>0){
+		while(i<N){
+			while(j<M){
+				cout<<"O";
+				j++;
+			}
+			cout<<endl;
+			j=0;
+			i++;
+		}
+	}else{
+		cout<<"Invalid input"<<endl;
+	}
+}
 int main(){
 	
 	printO(2,2);
@@ -17,7 +31,7 @@ int main(){
 	printO(0,3);
 	cout << "\n";	
 	
-	printO(7,-1);
+	printO(7,0);
 	cout << "\n";	
 	
 	return 0;
